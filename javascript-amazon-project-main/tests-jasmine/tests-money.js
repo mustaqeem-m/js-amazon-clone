d543cc7b9f7d2962ad7303adf1f42605de7ab80a
Md_Mustaqeeem <mmmustaqeem1910@gmail.com>
2025-04-21 22:34:47 +0530

    Remove default test and add money tests

---
+++ javascript-amazon-project-main/tests-jasmine/tests-money.js
@@ -0,0 +1,19 @@
+import { currencyFormatter } from "../Scripts/utils/money.js";
+
+describe("Currency Test_suite", () => {
+    it("converts cents to dollars", () => {
+        expect(currencyFormatter(2025)).toEqual("20.25");
+    });
+
+    it('works with 0: ', () => {
+        expect(currencyFormatter(0)).toEqual('0.00');
+    });
+
+    it('works with negative numbers: ', () => {
+        expect(currencyFormatter(-2025)).toEqual('-20.25');
+    });
+    // it('edge cases', () => {
+    //     expect(currencyFormatter(1234567890).toEqual('1,234,567.89'))
+    // })
+});
+
